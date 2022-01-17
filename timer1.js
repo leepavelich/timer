f21502d647f84d110491f8d694e38286b37cf686
Lee Pavelich <leepavelich@gmail.com>
2022-01-17 20:46:26 +0000

    Linted

--- timer1.js
+++ timer1.js
@@ -3,9 +3,9 @@ const times = process.argv
   .map(x => Number(x))
   .filter(x => x >= 0)
   .filter(x => typeof x === 'number')
-  .map(x => x * 10)
+  .map(x => x * 10);
 
-if (!times.length) return
+if (!times.length) return;
 
 for (let time of times) {
   setTimeout(() => process.stdout.write('\x07'), time);
