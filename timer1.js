50b5ade2f736cbd2726c30f090847181e9ae6aaa
Lee Pavelich <leepavelich@gmail.com>
2022-01-17 20:33:36 +0000

    Implemented basic timer, accomodating edge cases

---
+++ timer1.js
@@ -0,0 +1,12 @@
+const times = process.argv
+  .slice(2)
+  .map(x => Number(x))
+  .filter(x => x >= 0)
+  .filter(x => typeof x === 'number')
+  .map(x => x * 10)
+
+if (!times.length) return
+
+for (let time of times) {
+  setTimeout(() => process.stdout.write('\x07'), time);
+}
