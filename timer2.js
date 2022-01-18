c9202cf41e315e074289c6b5658ca1c13ee87cac
Lee Pavelich <leepavelich@gmail.com>
2022-01-18 02:43:44 +0000

    Implemented a timer with user input

---
+++ timer2.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+process.stdin.setRawMode(true);
+process.stdin.setEncoding('utf8');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Please enter a value: ', answer => {
+  if (answer === 'b') process.stdout.write('\x07')
+  if (Number(answer) > 0 && Number(answer) < 10) {
+    console.log('setting timer for x seconds...')
+    setTimeout(() => process.stdout.write('\x07'), answer * 1000)
+
+    process.on('SIGINT', () => {
+      console.log('Thanks for using me, ciao!')
+      process.exit();
+    });
+  };
+  rl.close()
+})
