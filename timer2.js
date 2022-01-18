e3ee63cf0b233c63211ffe311d2396f3bf1d85d2
Lee Pavelich <leepavelich@gmail.com>
2022-01-18 05:20:48 +0000

    Linted

--- timer2.js
+++ timer2.js
@@ -6,15 +6,15 @@ const rl = readline.createInterface({
 });
 
 rl.question('Please enter a value: ', answer => {
-  if (answer === 'b') process.stdout.write('\x07')
+  if (answer === 'b') process.stdout.write('\x07');
   if (Number(answer) > 0 && Number(answer) < 10) {
-    console.log('setting timer for x seconds...')
-    setTimeout(() => process.stdout.write('\x07'), answer * 1000)
+    console.log('setting timer for x seconds...');
+    setTimeout(() => process.stdout.write('\x07'), answer * 1000);
 
     process.on('SIGINT', () => {
-      console.log('Thanks for using me, ciao!')
+      console.log('Thanks for using me, ciao!');
       process.exit();
     });
-  };
-  rl.close()
-})
+  }
+  rl.close();
+});
